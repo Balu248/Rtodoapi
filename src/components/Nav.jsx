53b5330578cc9f3c472a69d3e09c762e987c62d7
Balu248 <balachandran7236@gmail.com>
2025-07-15 09:45:06 +0530

    todo api

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,22 +4,22 @@ import { Link } from 'react-router-dom'
 const Nav = () => {
   return (
     <div>
-        <nav class="navbar navbar-expand-lg bg-primary navbar-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">TO DO ✅</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <Link class="nav-link active" aria-current="page" to="#">Add TO DOs</Link>
-        <Link class="nav-link" to="/s">Search TO DOs</Link>
-        <Link class="nav-link" to="/d">Delete TO DOs</Link>
-        <Link class="nav-link" to="/v">View TO DOs</Link>
-      </div>
-    </div>
-  </div>
-</nav>
+      <nav class="navbar navbar-expand-lg bg-primary navbar-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">TO DO ✅</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <Link class="nav-link active" aria-current="page" to="#">Add TO DOs</Link>
+              <Link class="nav-link" to="/s">Search TO DOs</Link>
+              <Link class="nav-link" to="/d">Delete TO DOs</Link>
+              <Link class="nav-link" to="/v">View TO DOs</Link>
+            </div>
+          </div>
+        </div>
+      </nav>
     </div>
   )
 }
